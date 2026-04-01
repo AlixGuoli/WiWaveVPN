892858c6eb79c81bf7a402522c9d991c34d25b09
AlixGuoli <1425300103@qq.com>
2026-04-01 15:45:13 +0800

    Initial Commit

---
+++ WiWaveVPN/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WiWaveVPN
+//
+//  Created by ersao on 2026/4/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WiWaveVPN/WiWaveVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiWaveVPNApp.swift
+//  WiWaveVPN
+//
+//  Created by ersao on 2026/4/1.
+//
+
+import SwiftUI
+
+@main
+struct WiWaveVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
